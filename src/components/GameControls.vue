a92c7c91ce64f6af27dfc3baba09b5165922721a
ArGJolan <argjolan@gmail.com>
2020-05-29 20:21:33 +0100

    Actual controls

--- src/components/GameControls.vue
+++ src/components/GameControls.vue
@@ -1,7 +1,32 @@
 <template>
   <div
     class="game-controls"
-  />
+  >
+    <button
+      v-if="gameState === 'init'"
+      @click="startGame"
+    >
+      Start
+    </button>
+    <button
+      v-if="gameState === 'running'"
+      @click="resetGame"
+    >
+      Reset
+    </button>
+    <button
+      v-if="gameState === 'won'"
+      @click="resetGame"
+    >
+      Play Again!
+    </button>
+    <button
+      v-if="gameState === 'lost'"
+      @click="resetGame"
+    >
+      Try Again!
+    </button>
+  </div>
 </template>
 
 <script>
@@ -13,9 +38,20 @@ export default {
       required: true,
     },
   },
+
+  methods: {
+    startGame () {
+      this.$emit('start');
+    },
+    resetGame () {
+      this.$emit('reset');
+    },
+  },
 };
 </script>
 
 <style>
-
+.game-controls {
+  margin-top: 2rem;
+}
 </style>
